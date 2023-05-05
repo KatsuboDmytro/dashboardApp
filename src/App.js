0a6266f0daf77ec6c34c7de05a55e4ad6b1a2a28
KatsuboDmytro <dmitriyofficial03@gmaik.com>
2023-05-06 01:00:02 +0300

    pages and data

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/Sidebar";
-import Dashboard from "./scenes/dashboard";
-import {Team} from "./scenes/team";
-import Invoices from "./scenes/invoices";
-import Contacts from "./scenes/contacts";
-import Bar from "./scenes/bar";
-import Form from "./scenes/form";
-import Line from "./scenes/line";
-import Pie from "./scenes/pie";
-import FAQ from "./scenes/faq";
-import Geography from "./scenes/geography";
+import Dashboard from "./scenes/Dashboard";
+import {Team} from "./scenes/Team";
+import Invoices from "./scenes/Invoices";
+import Contacts from "./scenes/Contacts";
+import Bar from "./scenes/Bar";
+import Form from "./scenes/Form";
+import Line from "./scenes/Line";
+import Pie from "./scenes/Pie";
+import FAQ from "./scenes/FAQ";
+import Geography from "./scenes/Geography";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
-import Calendar from "./scenes/calendar";
+import Calendar from "./scenes/Calendar";
 
 function App() {
     const [theme, colorMode] = useMode();
--- src/scenes/Team.jsx
+++ src/scenes/Team.jsx
@@ -1,11 +1,11 @@
 import { Box, Typography, useTheme } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
-import { tokens } from "../../theme";
-import { mockDataTeam } from "../../data/mockData";
+import { tokens } from "../theme";
+import { mockDataTeam } from "../data/mockData";
 import AdminPanelSettingsOutlinedIcon from "@mui/icons-material/AdminPanelSettingsOutlined";
 import LockOpenOutlinedIcon from "@mui/icons-material/LockOpenOutlined";
 import SecurityOutlinedIcon from "@mui/icons-material/SecurityOutlined";
-import {Header} from "../../components/Header";
+import {Header} from "../components/Header";
 
 export const Team = () => {
   const theme = useTheme();
